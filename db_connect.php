9e401529faff77f62c11c7e3f23170201040f72a
Muhammad Raza Adzani <163233947+Razaaaaa27@users.noreply.github.com>
2024-10-09 11:59:05 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$host = ' 10.117.1.64';
+$host = '172.17.0.2';
 $dbname = 'global_master';
 $username = 'root';
 $password = '';
@@ -8,5 +8,5 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Koneksi database gagal: " . $e->getMessage() . " | Host: $host");
+    die("Koneksi database gagal: " . $e->getMessage());
 }
