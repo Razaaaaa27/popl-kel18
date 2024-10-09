45a321361c74bcdc3abccb61781f8abd6d182474
Muhammad Raza Adzani <163233947+Razaaaaa27@users.noreply.github.com>
2024-10-09 16:51:21 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,14 +1,14 @@
 <?php
-$host = '127.0.0.1'; // Nama service MySQL di docker-compose
+$host = 'mysql'; // Nama service MySQL yang Anda definisikan di docker-compose
 $dbname = 'global_master';
-$DB_CONNECTION = 'mysql';
 $username = 'root';
-$password = 'your_password'; // Ganti dengan password yang aman
+$password = 'your_password'; // Ganti dengan password yang sesuai
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname",$DB_CONNECTION $username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
 }
+?>
