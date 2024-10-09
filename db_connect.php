a65ba8df6cf038d3afc24b0c6c0cc481161fe2c9
Muhammad Raza Adzani <163233947+Razaaaaa27@users.noreply.github.com>
2024-10-09 17:08:01 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -7,7 +7,7 @@ $password = 'your_password'; // Ganti dengan password yang sesuai
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    // echo "Connected successfully";
 } catch (PDOException $e) {
     die("Koneksi database gagal: " . $e->getMessage());
 }
